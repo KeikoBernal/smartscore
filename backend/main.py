5df902c92c82d76d511d5c238aa38f26f228facc
Keiko Bernal <tu@correo.com~>
2025-08-18 19:41:46 -0400

    feat: reinicio del proyecto

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from routers import metrics, files
-
-app = FastAPI(title="SmartScore API")
-app.include_router(files.router, prefix="/api/files")
-app.include_router(metrics.router, prefix="/api/metrics")
--- backend/routers/upload.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-router = APIRouter(prefix="/upload", tags=["upload"])
-
-@router.post("/")
-async def upload_score(score_file: UploadFile = File(...)):
-    return {"filename": score_file.filename}
