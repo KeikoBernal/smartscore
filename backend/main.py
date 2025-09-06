3cd994e66d843fd502e041041cc630de84138479
Keiko Bernal <tu@correo.com~>
2025-09-05 21:39:06 -0400

    Correcciones finales

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app.include_router(clasificador_metricas_endpoint.router, prefix="/metricas")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://smartscoreanalyzer.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
